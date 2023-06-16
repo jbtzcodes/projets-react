1423764c8fa16e71a3412332a2b0ecbfc0d921a1
jbtzcodes <jonahant.benitez.contact@gmail.com>
2023-06-16 16:02:08 +0200

    Modification projet youtube clone

--- 14-youtube-clone/src/App.js
+++ 14-youtube-clone/src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Box} from '@mui/material'
+import {Box} from '@mui/material';
+
+import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components'
 
 const App = ()=> (
   <BrowserRouter>
   <Box sx={{backgroundColor: '#000'}}>
+    <Navbar />
     <Routes>
       <Route path='/' exact element={< Feed/>} />
-      <Route path='/video/:id' exact element={< VideoDetail/>} />
+      <Route path='/video/:id' element={< VideoDetails/>} />
+      <Route path='/channel/:id' element={< ChannelDetails/>} />
+      <Route path='/search/:searchTerm'  element={< SearchFeed/>} />
 
     </Routes>
   </Box>
---
+++ 14-youtube-clone/src/components/ChannelDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChannelDetails = () => {
+  return (
+    <div>channelDetails</div>
+  )
+}
+
+export default ChannelDetails
